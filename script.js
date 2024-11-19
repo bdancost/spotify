b2cc144218287f4c79c93182974fcbf4617ada17
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-19 19:52:15 -0300

    Configurando botao shuffle de embaralhar

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const next = document.getElementById("next"); // Botão next
 const previous = document.getElementById("previous"); // Botão previous
 const currentProgress = document.getElementById("current-progress");
 const progressContainer = document.getElementById("progress-container");
+const shuffleButton = document.getElementById("shuffle");
 
 const wayMuchBetter = {
   songName: "Way Much Better",
@@ -32,7 +33,9 @@ const shivers = {
 };
 
 let isPlaying = false;
-const playlist = [wayMuchBetter, demons, shivers];
+let isShuffle = false;
+const originalPlaylist = [wayMuchBetter, demons, shivers];
+let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
 function playSong() {
@@ -58,16 +61,16 @@ function playPauseDecider() {
 }
 
 function initializeSong() {
-  const imagePath = `./image/${playlist[index].file}.${playlist[index].imageExt}`;
+  const imagePath = `./image/${sortedPlaylist[index].file}.${sortedPlaylist[index].imageExt}`;
   cover.src = imagePath;
-  song.src = `./music/${playlist[index].file}.mp3`;
-  songName.innerText = playlist[index].songName;
-  bandName.innerText = playlist[index].artist;
+  song.src = `./music/${sortedPlaylist[index].file}.mp3`;
+  songName.innerText = sortedPlaylist[index].songName;
+  bandName.innerText = sortedPlaylist[index].artist;
 }
 
 function previousSong() {
   if (index === 0) {
-    index = playlist.length - 1;
+    index = sortedPlaylist.length - 1;
   } else {
     index -= 1;
   }
@@ -76,7 +79,7 @@ function previousSong() {
 }
 
 function nextSong() {
-  if (index === playlist.length - 1) {
+  if (index === sortedPlaylist.length - 1) {
     index = 0;
   } else {
     index += 1;
@@ -97,6 +100,30 @@ function jumpTo(event) {
   song.currentTime = jumpToTime;
 }
 
+function shuffleArray(preShuffleArray) {
+  const size = preShuffleArray.length;
+  let currentIndex = size - 1;
+  while (currentIndex > 0) {
+    let randomIndex = Math.floor(Math.random() * size);
+    let aux = preShuffleArray[currentIndex];
+    preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+    preShuffleArray[randomIndex] = aux;
+    currentIndex -= 1;
+  }
+}
+
+function shuffleButtonClicked() {
+  if (isShuffle === false) {
+    isShuffle = true;
+    shuffleArray(sortedPlaylist);
+    shuffleButton.classList.add("button-active");
+  } else {
+    isShuffle = false;
+    sortedPlaylist = [...originalPlaylist];
+    shuffleButton.classList.remove("button-active");
+  }
+}
+
 initializeSong();
 
 play.addEventListener("click", playPauseDecider);
@@ -104,3 +131,4 @@ previous.addEventListener("click", previousSong);
 next.addEventListener("click", nextSong);
 song.addEventListener("timeupdate", updateProgressBar);
 progressContainer.addEventListener("click", jumpTo);
+shuffleButton.addEventListener("click", shuffleButtonClicked);
